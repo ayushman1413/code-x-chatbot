b844e4ae136cb41430bcc9a97eb14dc60241e506
ayushman1413 <ayushman1413@gmail.com>
2025-11-19 15:19:19 +0530

    update

--- src/Home.jsx
+++ src/Home.jsx
@@ -58,6 +58,7 @@ const Home = () => {
       </section>
 
 
+
       <section id="contact" className="contact">
         <h2>Contact Us</h2>
         <p>Get in touch with us for your next project.</p>
